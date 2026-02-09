a8da13f39faf453fc460699c39266133722fdf9f
Marina Pak <marrinna03@mail.ru>
2026-02-09 18:22:35 +0300

    feat: добавлена разметка страницы 404

--- src/pages/not-found/NotFoundPage.tsx
+++ src/pages/not-found/NotFoundPage.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Button, Result } from 'antd';
+import {useNavigate} from "react-router-dom";
 
 export const NotFoundPage: React.FC = () => {
+  const navigate = useNavigate()
+
   return (
-    <>NotFoundPage</>
+    <Result
+      status="404"
+      title="404"
+      subTitle="Извините, страница, которую вы посетили, не существует."
+      extra={
+      <Button
+        onClick={() => navigate('/login')}
+        type="primary">На главную</Button>}
+    />
   )
 }
